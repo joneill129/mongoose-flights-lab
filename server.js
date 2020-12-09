f4817d27216ee0b4bd35f2be95242a82865ee784
jpaige343 <jenniferoneill129@gmail.com>
2020-12-08 20:22:41 -0600

    heroku troubleshoot

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const methodOverride = require("method-override");
 
 require('dotenv').config();
+
+const app = express();
+
 require('./config/database');
 
 const indexRouter = require('./routes/index');
@@ -13,17 +16,18 @@ const flightsRouter = require('./routes/flights');
 const ticketsRouter = require('./routes/tickets');
 const destinationsRouter = require('./routes/destinations');
 
-const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', indexRouter);
 app.use('/flights', flightsRouter);
